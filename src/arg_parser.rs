e1793a9e07ccc259d5607eba44166375c70b897a
Nariod <dorian.p@protonmail.ch>
2023-08-03 09:57:50 +0200

    Moved to clap V4

--- src/arg_parser.rs
+++ src/arg_parser.rs
@@ -1,8 +1,7 @@
 // Module that handles the CLI arguments and checks them for correct values.
 
-use clap::{Arg, ArgMatches, Command, PossibleValue};
+use clap::{Arg, ArgMatches, Command};
 use std::path::PathBuf;
-use std::str::FromStr;
 
 use crate::tools::absolute_path;
 
@@ -10,7 +9,7 @@ use crate::tools::absolute_path;
 pub struct Order {
     pub shellcode_path: PathBuf,
     pub execution: Execution,
-    pub encryption: Option<Encryption>,
+    pub encryption: Encryption,
     pub format: Format,
     pub target_process: String,
     //sandbox: Option<bool>,
@@ -40,83 +39,78 @@ pub enum Format {
 }
 
 fn parser() -> ArgMatches {
-    let args = Command::new("RustPacker")
+    Command::new("RustPacker")
         .author("by Nariod")
         .version("0.10")
         .about("Shellcode packer written in Rust.")
         .arg_required_else_help(true)
+        .arg(Arg::new("Path to shellcode file").short('f').required(true))
         .arg(
-            Arg::with_name("Path to shellcode file")
-                .takes_value(true)
-                .short('f')
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("Binary output format")
-                .takes_value(true)
+            Arg::new("Binary output format")
                 .short('b')
                 .required(true)
                 .value_parser([
-                    PossibleValue::new("exe").help("EXE format"),
-                    PossibleValue::new("dll").help("DLL format"),
+                    clap::builder::PossibleValue::new("exe").help("EXE format"),
+                    clap::builder::PossibleValue::new("dll").help("DLL format"),
                 ]),
         )
         .arg(
-            Arg::with_name("Execution technique")
-                .takes_value(true)
+            Arg::new("Execution technique")
                 .short('i')
                 .required(true)
                 .value_parser([
                     // PossibleValue::new("ct").help("Create Thread"),
                     // PossibleValue::new("crt").help("Create Remote Thread"),
-                    PossibleValue::new("ntapc").help("Self inject using APC low level APIs"),
-                    PossibleValue::new("ntcrt").help("Create Remote Thread using low level APIs"),
-                    PossibleValue::new("syscrt").help("Create Remote Thread using syscalls"),
-                    PossibleValue::new("wincrt")
+                    clap::builder::PossibleValue::new("ntapc")
+                        .help("Self inject using APC low level APIs"),
+                    clap::builder::PossibleValue::new("ntcrt")
+                        .help("Create Remote Thread using low level APIs"),
+                    clap::builder::PossibleValue::new("syscrt")
+                        .help("Create Remote Thread using syscalls"),
+                    clap::builder::PossibleValue::new("wincrt")
                         .help("Create Remote Thread using the official Windows Crate"),
                 ]),
         )
         .arg(
-            Arg::with_name("Target process")
-                .takes_value(true)
-                .short('t')
-                .help(
-                    "Target processes to inject into, defaults to 'dllhost.exe'. Case sensitive!",
-                ),
+            Arg::new("Target process").short('t').help(
+                "Target processes to inject into, defaults to 'dllhost.exe'. Case sensitive!",
+            ),
         )
         .arg(
-            Arg::with_name("Encryption / encoding method")
-                .takes_value(true)
+            Arg::new("Encryption / encoding method")
                 .short('e')
                 .required(true)
                 .value_parser([
-                    PossibleValue::new("xor").help("'Exclusive or' encoding"),
-                    PossibleValue::new("aes").help("AES 256 encryption"),
+                    clap::builder::PossibleValue::new("xor").help("'Exclusive or' encoding"),
+                    clap::builder::PossibleValue::new("aes").help("AES 256 encryption"),
                 ]),
         )
-        .get_matches();
-
-    args
+        .get_matches()
 }
 
 fn args_checker(args: ArgMatches) -> Result<Order, Box<dyn std::error::Error>> {
-    let sp: String = String::from_str(args.value_of("Path to shellcode file").unwrap())?;
+    let sp: String = args
+        .get_one::<String>("Path to shellcode file")
+        .unwrap()
+        .clone();
     let relative_shellcode_path: PathBuf = [sp].iter().collect();
     let shellcode_path = match absolute_path(relative_shellcode_path) {
         Ok(path) => path,
         Err(err) => panic!("{:?}", err),
     };
 
-    let encryption = match args.value_of("Encryption / encoding method") {
-        Some("xor") => Some(Encryption::Xor),
-        Some("aes") => Some(Encryption::Aes),
+    let s = args
+        .get_one::<String>("Encryption / encoding method")
+        .unwrap();
+    let encryption: Encryption = match s.as_str() {
+        "xor" => Encryption::Xor,
+        "aes" => Encryption::Aes,
         _ => panic!("Don't even know how this error exists."),
     };
-
     //let sandbox: Option<bool> = None;
     //let output: Option<String> = None;
 
-    let s = String::from_str(args.value_of("Execution technique").unwrap())?;
+    let s = args.get_one::<String>("Execution technique").unwrap();
     let execution: Execution = match s.as_str() {
         // "ct" => Execution::CreateThread,
         // "crt" => Execution::CreateRemoteThread,
@@ -127,14 +121,14 @@ fn args_checker(args: ArgMatches) -> Result<Order, Box<dyn std::error::Error>> {
         _ => panic!("Don't even know how this error exists."),
     };
 
-    let s = String::from_str(args.value_of("Binary output format").unwrap())?;
+    let s = args.get_one::<String>("Binary output format").unwrap();
     let format: Format = match s.as_str() {
         "exe" => Format::Exe,
         "dll" => Format::Dll,
         _ => panic!("Don't even know how this error exists."),
     };
 
-    let target_process = match args.value_of("Target process") {
+    let target_process = match args.get_one::<String>("Target process") {
         Some(name) => name.to_string(),
         None => "dllhost.exe".to_string(),
     };

